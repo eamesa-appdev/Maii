42c6e7dc18d50b06bc326c4ffb3a8cb8d4bd5384
eamesa <eamadorm@chicagobooth.edu>
2022-03-15 04:03:45 +0000

    loaded api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
 
   def homepage
+
+    api = "https://www.datos.gov.co/resource/qhpu-8ixx.json"
+    raw_data = open(api).read
+    @parsed_data = JSON.parse(raw_data)
+
+
+
     render ({:template => "home.html.erb"})
   end
 
